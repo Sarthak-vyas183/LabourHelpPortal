f730d67aa7caed0c2021c2496edabd8efb9aabf5
Sarthak vyas <142726436+Sarthak-vyas183@users.noreply.github.com>
2024-04-07 02:09:04 +0530

    service page completed

--- services.php
+++ services.php
@@ -6,13 +6,123 @@
     <title>About</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="design.css">
+     <link rel="stylesheet" href="services.css">
+     <link rel="stylesheet" href="input.css">
+     <link href="output.css" rel="stylesheet">
 </head>
 <body>
    <?php
    require("Navbar.php");
    ?>
-   <div id="main">
-    <h1> Services section </h1>
+   <div class="sr-main">
+
+   <div class="card" style="width: 18rem;">
+     <img src="./partials/painter.jpg" class="card-img-top" alt="...">
+
+     <div class="card-body">
+      <h5 class="card-title">Painter</h5>
+      <p class="card-text">Color Your World with Our Expertise : Call Painter</p>
+      <a href="#" class="btn btn-primary">Book Painter 📞</a>
+     </div>
+  </div>
+   <div class="card" style="width: 18rem;">
+     <img src="./partials/construction_labour.jpg" class="card-img-top" alt="...">
+
+     <div class="card-body">
+      <h5 class="card-title">General Labour</h5>
+        <p class="card-text">Book a labour for Genral labor work , cleaning ,unloading materials</p>
+      <a href="/" class="btn btn-primary">Book Worker📞</a>
+     </div>
+  </div>
+   
+   <div class="card" style="width: 18rem;">
+     <img src="./partials/welder.jpg" class="card-img-top" alt="...">
+
+     <div class="card-body">
+      <h5 class="card-title">Welder</h5>
+      <p class="card-text">Spark Your Imagination, Welding Your Vision : Call Welder.</p>
+      <a href="#" class="btn btn-primary">Book Welder📞</a>
+     </div>
+
+    </div>
+
+   <div class="card" style="width: 18rem;">
+     <img src="./partials/AC-Repair.png" class="card-img-top" alt="...">
+
+     <div class="card-body">
+      <h5 class="card-title">Technician</h5>
+      <p class="card-text">Home Appliance Repair: Call Technician</p>
+      <a href="/" class="btn btn-primary">Book Technician📞</a>
+     </div>
+
+    </div>
+
+   <div class="card" style="width: 18rem;">
+     <img src="./partials/electrician.jpg" class="card-img-top" alt="...">
+
+     <div class="card-body">
+      <h5 class="card-title">Electrician</h5>
+      <p class="card-text">Shocking Quality, Electrifying Service : Call Electrician</p>
+      <a href="/" class="btn btn-primary">Book electrician📞</a>
+     </div>
+
+    </div>
+
+   <div class="card" style="width: 18rem;">
+     <img src="./partials/plumber.png" class="card-img-top" alt="...">
+
+     <div class="card-body">
+      <h5 class="card-title">plumber</h5>
+      <p class="card-text">Leak Problems? Wea have Got the Solution : Call Plumber</p>
+      <a href="#" class="btn btn-primary">Book Plumber📞</a>
+     </div>
+
+    </div>
+
+   <div class="card" style="width: 18rem;">
+     <img src="./partials/barber.jpg" class="card-img-top" alt="...">
+
+     <div class="card-body">
+      <h5 class="card-title">Barber</h5>
+      <p class="card-text">Your Look, Our Expertise!: Call Barber</p>
+      <a href="/" class="btn btn-primary">Book Barber📞</a>
+     </div>
+
+    </div>
+
+   <div class="card" style="width: 18rem;">
+     <img src="./partials/Chef.jpg" class="card-img-top" alt="...">
+
+     <div class="card-body">
+      <h5 class="card-title">Chef</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+     </div>
+
+    </div>
+
+   <div class="card" style="width: 18rem;">
+     <img src="./partials/farming_labour.jpg" class="card-img-top" alt="...">
+
+     <div class="card-body">
+      <h5 class="card-title">Farming Labour</h5>
+      <p class="card-text">Where Hard Work Meets Heart for the Harvest: Call farming labour</p>
+      <a href="#" class="btn btn-primary">Book Labour📞</a>
+     </div>
+
+    </div>
+
+   <div class="card" style="width: 18rem;">
+     <img src="./partials/construction_labour.jpg" class="card-img-top" alt="...">
+
+     <div class="card-body">
+      <h5 class="card-title">Construction Labour</h5>
+       <p class="card-text">Book a labour for Genral labor work ,Construction work,unloading materials</p>
+      <a href="#" class="btn btn-primary">Book Worker</a>
+     </div>
+
+    </div>
+
    </div>
    <?php
    require("footer.php");
